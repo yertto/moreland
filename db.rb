4b241d927e4d3d9bb058c036e5f4c2dad3aa4d27
yertto <yertto@gmail.com>
2010-06-29 13:53:32 +1000

    validations..

--- db.rb
+++ db.rb
@@ -9,8 +9,8 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3:///#{Dir.pwd}/devel.d
 
 class ApplicationNumber < DataMapper::Type  # NB. causes an erroneous deprecation warning in dm v1.0.0
   primitive       String
-  #length          8    # XXX - sqlite is ignoring this :(
-  #auto_validation true # XXX - sqlite is ignoring this :(
+  length          8    # XXX - sqlite is ignoring this :(
+  auto_validation true # XXX - sqlite is ignoring this :(
 
   def self.re
     @re ||= Regexp.compile('^((MPS|SP|SC|MIN)/\d{4}/\d+(?:/([A-K]))?)$')
